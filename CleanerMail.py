620bdbadc599cb1450fc65461dea9c10b8bdd13e
Mike Evers <m.evers95@gmail.com>
2019-10-20 22:22:34 +0200

    Add HTML email

--- CleanerMail.py
+++ CleanerMail.py
@@ -42,14 +42,92 @@ def send_CleanerMail():
             '\nVloer: '+cleaners["floor"]+
             '\nOppervlaktes: '+cleaners["surface"]
             )
-        # msg.add_alternative("""\
-        # <!DOCTYPE html>
-        # <html>
-        #     <body>
-        #         <h1 style="color:SlateGray;">This is an HTML Email!</h1>
-        #     </body>
-        # </html>
-        #     """, subtype='html')
+        msg.add_alternative("""\
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Club Huyzch Schoonmaakrooster</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body style="margin: 0; padding: 0; text-align: center;">
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">	
+		<tr>
+			<td style="padding: 10px 0 30px 0;">
+				<table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border: 1px solid #cccccc; border-collapse: collapse;">
+					<tr>
+						<td align="center" bgcolor="#153643" style="padding: 30px 0 30px 0; color: #153643; font-size: 28px; font-weight: bold; font-family: Arial, sans-serif;">
+							<img src="http://www.clubhuyzch.nl/images/logo_white-2x.png" alt="Club Huyzch logo" width="200" style="display: block;" />
+						</td>
+					</tr>
+					<tr>
+						<td bgcolor="#ffffff" style="padding: 40px 30px 30px 30px;">
+							<table border="0" cellpadding="0" cellspacing="0" width="100%">
+								<tr>
+									<td style="color: #43153b; font-family: Arial, sans-serif;text-align: center; font-size: 30px;">
+										<b>Schoonmaakrooster</b>
+									</td>
+								</tr>
+								<tr>
+									<td style="padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+										Halloooo Huyzch bewoners, er mag weer lekker geschrobt en geboend worden. Hieronder staan de schoonmakers voor week """+str(date.today().isocalendar()[1])+""".
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<table border="0" cellpadding="0" cellspacing="0" width="100%">
+											<tr>
+												<td width="260" valign="top">
+													<table border="0" cellpadding="0" cellspacing="0" width="100%">
+														<tr>
+															<td>
+                                                                <h2 style="margin: 0px;">Vloeren</h2>
+															</td>
+														</tr>
+														<tr>
+															<td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+																 """+cleaners["floor"]+"""
+															</td>
+														</tr>
+													</table>
+												</td>
+												<td style="font-size: 0; line-height: 0;" width="20">
+													&nbsp;
+												</td>
+												<td width="260" valign="top">
+													<table border="0" cellpadding="0" cellspacing="0" width="100%">
+														<tr>
+															<td>
+																<h2 style="margin: 0px;">Oppenvlaktes</h2>
+															</td>
+														</tr>
+														<tr>
+															<td style="color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+																"""+cleaners["surface"]+"""
+															</td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+										</table>
+									</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding-top: 40px;">
+                                        <p style="margin: 0px;"><i><b>Vloeren:</b> Stofzuigen en dweilen van de gang, keuken en woonkamer. (en nee niet de gang dweilen)</i></p>
+                                        <p style="margin: 0px;"><i><b>Oppervlaktes:</b> Opruimen van de keuken en woonkamer, en het doen van de huiswas.</i></p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
+            """, subtype='html')
         smtp.send_message(msg)
         print("Email sent.")
 
